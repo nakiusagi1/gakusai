1b556043d6b4407538dfa02713a80eec7f244193
nakiusagi1 <125706134+nakiusagi1@users.noreply.github.com>
2023-07-21 17:58:59 +0900

    Add files via upload

---
+++ image_upload_2/loading.js
@@ -0,0 +1,9 @@
+// ローディングアニメーションの表示・非表示
+function showLoadingAnimation() {
+	var loading = document.getElementById("loading");
+	loading.style.display = "block";
+}
+function hideLoadingAnimation() {
+	var loading = document.getElementById("loading");
+	loading.style.display = "none";
+}
